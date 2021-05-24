b297a9833027df9c7cf229cf8c72b5051d67801b
Macbook <tin.caotrong@gmail.com>
2021-05-24 23:08:08 +0700

    implement notification handler

--- repository/recipeViews.repo.js
+++ repository/recipeViews.repo.js
@@ -7,26 +7,28 @@ const { Op } = require('sequelize');
 const e = require("express");
 
 async function countView(recipe_id) {
-    const recipe = await Recipe.findOne({ where: { id: recipe_id }});
-    if (!recipe) {
-        throw Error('Recipe not found')
-    }
+    // const recipe = await Recipe.findOne({ where: { id: recipe_id }});
+    // if (!recipe) {
+    //     throw Error('Recipe not found')
+    // }
 
-    const recipeView = await RecipeViews.findOne({
-        where: {
-            recipe_id
-        }
-    })
-    if (!recipeView) {
-        return RecipeViews.create({ recipe_id, count: 1})
-    } else {
-        const viewsOfRecipe = recipeView.count;
-        return RecipeViews.update({ count: viewsOfRecipe + 1}, {
-            where: {
-                id: recipeView.id
-            }
-        })
-    }
+    // const recipeView = await RecipeViews.findOne({
+    //     where: {
+    //         recipe_id
+    //     }
+    // })
+    // if (!recipeView) {
+    //     return RecipeViews.create({ recipe_id, count: 1})
+    // } else {
+    //     const viewsOfRecipe = recipeView.count;
+    //     return RecipeViews.update({ count: viewsOfRecipe + 1}, {
+    //         where: {
+    //             id: recipeView.id
+    //         }
+    //     })
+    // } 
+    const recipeView = await RecipeViews.create({ recipe_id, count: 1 })
+    return recipeView;
 }
 
 async function getViewsOfRecipe(recipe_id) {
