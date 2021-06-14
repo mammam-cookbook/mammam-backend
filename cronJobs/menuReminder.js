63d0aa1bf3d3ec1beee6654f1a79524334a03a5c
Macbook <tin.caotrong@gmail.com>
2021-06-14 23:31:58 +0700

    test

---
+++ cronJobs/menuReminder.js
@@ -0,0 +1,8 @@
+const menuRepo = require("../repository/menu.repo");
+const recipeaRepo = require('../repository/recipe.repo')
+exports.remindRecipeInMenu = () => {
+    
+    // get recommend recipes of user
+
+    // send email
+}
