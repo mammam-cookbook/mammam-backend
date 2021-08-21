6d5b301546ed1e087e10f8ea13d6c5c9a682658e
tin <tin.caotrong@gmail.com>
2021-08-21 21:36:34 +0700

    update

--- routes/menu.route.js
+++ routes/menu.route.js
@@ -16,8 +16,6 @@ router.get("/", authorize, async function (req, res) {
   const { id: user_id } = req.user;
   let result = await menuRepo.getRecipesBetweenTwoDates(startDate, endDate, user_id);
   for (let i = 0; i < result.length - 1; i++) {
-    result[i] = result[i].toJSON();
-    result[i+1] = result[i+1].toJSON();
     console.log({ result: result[i], afterResult: result[i+1], i})
     if (result[i].timestamp === result[i+1].timestamp && timeValue[result[i].session] > timeValue[result[i+1].session]) {
       [result[i], result[i+1]] = [result[i+1], result[i]];
